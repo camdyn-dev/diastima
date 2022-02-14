9c475b7f84c6a75167dd77bb50e84a6316469966
Camdyn <camdyn3@protonmail.com>
2022-02-14 02:00:08 -0800

    refine positioning a little more, add a few images

--- main.js
+++ main.js
@@ -44,6 +44,13 @@ const jupiter = new THREE.Mesh(
   new THREE.SphereGeometry(110, 32, 32),
   new THREE.MeshStandardMaterial({ map: jupiterTexture })
 )
+
+const marsTexture = new THREE.TextureLoader().load("mars.jpg")
+const mars = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 32, 32),
+  new THREE.MeshStandardMaterial({ map: marsTexture })
+)
+
 jupiter.position.x = -500 //setting jupiter far away
 jupiter.position.z = -1000 //setting it far off to the right
 jupiter.position.y = 100 //setting it H I G H
@@ -52,22 +59,26 @@ earthOrbit.add(moon) //making the moon orbit earf
 earthOrbit.position.z = -1 //offsetting the earth a bit for the camera
 earth.position.z = -1 //same here
 
-moon.position.x = -50
-moon.position.y = 25
-moon.position.z = -5
+mars.position.z = -250
+mars.position.x = -150
+mars.position.y = -30
 
-scene.add(earth, earthOrbit, jupiter)
+moon.position.x = -50 //moon positioning
+moon.position.y = 30
+moon.position.z = -8
+
+scene.add(earth, earthOrbit, jupiter, mars)
 
 
-camera.position.set(35, 0, 10)
+camera.position.set(35, 0, 10) //LEAVE THIS, FOR SOME FUCKING REASON IT DOES N O T WORK WITHOUT IT, CHECK LINE 78 -> 82
 
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(250, 0, 0) //lightbulb. set it's position on the x y z axis
 scene.add(pointLight)
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(2000, 50) //this creates a grid
-scene.add(lightHelper, gridHelper) //shows where the point light is and what direction it points
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(2000, 50) //this creates a grid
+// scene.add(lightHelper, gridHelper) //shows where the point light is and what direction it points
 
 const controls = new OrbitControls(camera, renderer.domElement) //this creates a control for me to move around and view that grid in 35
 
@@ -75,11 +86,12 @@ const controls = new OrbitControls(camera, renderer.domElement) //this creates a
 const spaceTexture = new THREE.TextureLoader().load("space.jpg")
 scene.background = spaceTexture
 let t = document.body.getBoundingClientRect().top
+
 for (let i = 0; i < 1; i++) {
   camera.position.x = t * -.005 + 7
   camera.position.y = t * -.0001 + 0
   camera.position.z = t * -.0001 + 15
-}
+} //LEAVE THIS AS WELL, IT REMOVES THE BUG WHERE IT SNAPS INTO THE CORRECT POSITION UPON FIRST SCROLL
 
 
 //this lets me move the camera while it scrolls 
@@ -88,7 +100,7 @@ function moveCamera() {
   // moves the camera position
 
 
-  camera.position.x = t * -.005 + 7
+  camera.position.x = t * -.006 + 7
   camera.position.y = t * -.0001 + 0
   camera.position.z = t * -.0001 + 15
   console.log(camera.position)
@@ -105,6 +117,7 @@ const animate = () => {
   // torus.rotation.y += 0.01
   // torus.rotation.z += 0.01
   earth.rotateY(0.0005) // one rotation in 1 min 45 secs
+  mars.rotateY(0.00049)
   moon.rotateY(0.00001785) // now I think this should do one rotation every like 50 mins
   earthOrbit.rotateY(0.00001785)
   jupiter.rotateY(0.0012)
